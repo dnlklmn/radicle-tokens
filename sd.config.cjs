ee1b736589ac5b72524b494508078e53aded2ec8
dnlklmn <pelicansinberlin@gmail.com>
2023-08-24 12:41:14 +0200

    simplify style dictionary config

--- sd.config.cjs
+++ sd.config.cjs
@@ -3,23 +3,12 @@ const { registerTransforms } = require("@tokens-studio/sd-transforms");
 
 registerTransforms(StyleDictionary);
 
-StyleDictionary.registerFormat({
-  name: "css/variables",
-  formatter: function (dictionary) {
-    return `${this.selector} {\n${dictionary.allProperties
-      .map((prop) => `  --${prop.name}: ${prop.value};`)
-      .join("\n")}\n}`;
-  },
-});
-
 // generate css for each token set
 ["light", "dark", "global"].map((theme) => {
   const themeVariables = StyleDictionary.extend({
     source: [`src/tokens/${theme}.json`],
-    include: [`src/tokens/global.json`],
     platforms: {
-      web: {
-        transformGroup: "tokens-studio",
+      css: {
         transforms: [
           "ts/descriptionToComment",
           "ts/size/px",
@@ -41,9 +30,6 @@ StyleDictionary.registerFormat({
             format: "css/variables",
             selector:
               theme === "dark" ? `:root[data-theme="${theme}"]` : `:root`,
-            filter: ({ isSource }) => {
-              return isSource;
-            },
           },
         ],
       },
@@ -52,37 +38,3 @@ StyleDictionary.registerFormat({
   themeVariables.cleanAllPlatforms();
   themeVariables.buildAllPlatforms();
 });
-
-const globalVariables = StyleDictionary.extend({
-  source: [`src/tokens/global.json`],
-  include: [`src/tokens/global.json`],
-  platforms: {
-    web: {
-      transformGroup: "tokens-studio",
-      transforms: [
-        "ts/descriptionToComment",
-        "ts/size/px",
-        "ts/size/css/letterspacing",
-        "ts/size/lineheight",
-        "ts/type/fontWeight",
-        "ts/resolveMath",
-        "ts/typography/css/shorthand",
-        "ts/border/css/shorthand",
-        "ts/shadow/css/shorthand",
-        "ts/color/css/hexrgba",
-        "ts/color/modifiers",
-        "name/cti/kebab",
-      ],
-      buildPath: "./",
-      files: [
-        {
-          destination: `global.css`,
-          format: "css/variables",
-          selector: `:root`,
-        },
-      ],
-    },
-  },
-});
-globalVariables.cleanAllPlatforms();
-globalVariables.buildAllPlatforms();
